d66ddcec2495f7a504defe464cf239bf1822a03b
kwonjeong <kwonjeong9470@gmail.com>
2023-07-04 00:33:06 +0900

    [Setting] #1 - 프로젝트 생성

---
+++ Puzzling-iOS/Puzzling-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Puzzling-iOS
+//
+//  Created by KJ on 2023/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
