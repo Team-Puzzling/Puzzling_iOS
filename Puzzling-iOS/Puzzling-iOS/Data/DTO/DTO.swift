1f778bd116a41b5ab9ead591caa1402239650c97
kwonjeong <kwonjeong9470@gmail.com>
2023-07-04 01:08:07 +0900

    [Setting] #1 - 폴더링

---
+++ Puzzling-iOS/Puzzling-iOS/Data/DTO/DTO.swift
@@ -0,0 +1,8 @@
+//
+//  DTO.swift
+//  Puzzling-iOS
+//
+//  Created by KJ on 2023/07/04.
+//
+
+import Foundation
---
+++ Puzzling-iOS/Puzzling-iOS/Data/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Puzzling-iOS
+//
+//  Created by KJ on 2023/07/04.
+//
+
+import Foundation
