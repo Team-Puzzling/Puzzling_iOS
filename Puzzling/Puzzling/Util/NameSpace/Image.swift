be7e4b7396e43b3823109e6c57939b73b00dffb4
kwonjeong <kwonjeong9470@gmail.com>
2023-07-08 23:32:57 +0900

    [Setting] #1 - 탭바 이미지 에셋 수정

--- Puzzling/Puzzling/Util/NameSpace/Image.swift
+++ Puzzling/Puzzling/Util/NameSpace/Image.swift
@@ -5,4 +5,14 @@
 //  Created by KJ on 2023/07/04.
 //
 
-import Foundation
+import UIKit
+
+public enum Image {
+    
+    // MARK: - TabBar
+    
+    public static let home = UIImage(named: "home")!
+    public static let selectedHome = UIImage(named: "selectedHome")!
+    public static let myPage = UIImage(named: "myPage")!
+    public static let selectedMyPage = UIImage(named: "selectedMyPage")!
+}
