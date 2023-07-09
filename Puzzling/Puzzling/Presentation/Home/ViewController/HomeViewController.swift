5c9c90df4f7b214e27cc0deedbac94926dd122c6
kwonjeong <kwonjeong9470@gmail.com>
2023-07-10 02:12:52 +0900

    [Setting] #3 - 폰트 추가

--- Puzzling/Puzzling/Presentation/Home/ViewController/HomeViewController.swift
+++ Puzzling/Puzzling/Presentation/Home/ViewController/HomeViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "내 이름은 권정임"
+        label.font = .fontGuide(.heading1_kor)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("HomeViewController")
+        
+        setStyle()
+        setLayout()
+    }
+}
+
+private extension HomeViewController {
+    
+    func setStyle() {
+        
         view.backgroundColor = .white
+    }
+    
+    func setLayout() {
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+                                     nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50)])
     }
 
 
---
+++ Puzzling/Puzzling/Util/NameSpace/SFProType.swift
@@ -0,0 +1,15 @@
+//
+//  SFProType.swift
+//  Puzzling
+//
+//  Created by KJ on 2023/07/10.
+//
+
+import Foundation
+
+public enum SFProType: String {
+    
+    case black = "SF Pro Text Black"
+    case bold = "SF Pro Text Bold"
+    case regular = "SF Pro Text Regular"
+}
