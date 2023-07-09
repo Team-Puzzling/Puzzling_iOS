fdac00f7bfbfaed7185b5c33d842b833daba326b
kwonjeong <kwonjeong9470@gmail.com>
2023-07-10 02:42:23 +0900

    [Setting] #3 - Color 에셋 추가

--- Puzzling/Puzzling/Util/NameSpace/Color.swift
+++ Puzzling/Puzzling/Util/NameSpace/Color.swift
@@ -5,4 +5,47 @@
 //  Created by KJ on 2023/07/04.
 //
 
-import Foundation
+import UIKit
+
+public enum Color {
+    
+    // MARK: - Achromatic Color
+    
+    public static let black = UIColor(named: "black-000")!
+    public static let gray1 = UIColor(named: "gray-100")!
+    public static let gray2 = UIColor(named: "gray-200")!
+    public static let gray3 = UIColor(named: "gray-300")!
+    public static let gray4 = UIColor(named: "gray-400")!
+    public static let gray5 = UIColor(named: "gray-500")!
+    public static let gray6 = UIColor(named: "gray-600")!
+    public static let gray7 = UIColor(named: "gray-700")!
+    public static let white = UIColor(named: "white-000")!
+    
+    // MARK: - Background Color
+    
+    public static let bg050 = UIColor(named: "bg-050")!
+    public static let bg500 = UIColor(named: "bg-500")!
+    
+    // MARK: - Main Blue
+    
+    public static let blue7 = UIColor(named: "blue-700")!
+    public static let blue6 = UIColor(named: "blue-600")!
+    public static let blue5 = UIColor(named: "blue-500")!
+    public static let blue4 = UIColor(named: "blue-400")!
+    public static let blue3 = UIColor(named: "blue-300")!
+    public static let blue2 = UIColor(named: "blue-200")!
+    public static let blue1 = UIColor(named: "blue-100")!
+    
+    // MARK: - Main Yellow
+    
+    public static let yellow5 = UIColor(named: "yellow-500")!
+    public static let yellow4 = UIColor(named: "yellow-400")!
+    public static let yellow2 = UIColor(named: "yellow-200")!
+    public static let yellow1 = UIColor(named: "yellow-100")!
+    
+    // MARK: - Warning Red
+    
+    public static let red6 = UIColor(named: "red-600")!
+    public static let red4 = UIColor(named: "red-400")!
+    public static let red2 = UIColor(named: "red-200")!
+}
