7be25ac259dd8550d3171bd3c17aa335c0ed7fa7
kwonjeong <kwonjeong9470@gmail.com>
2023-07-10 02:22:42 +0900

    [Setting] #3 - 이미지 에셋 추가

--- Puzzling/Puzzling/Util/NameSpace/Image.swift
+++ Puzzling/Puzzling/Util/NameSpace/Image.swift
@@ -15,4 +15,19 @@ public enum Image {
     public static let selectedHome = UIImage(named: "selectedHome")!
     public static let myPage = UIImage(named: "myPage")!
     public static let selectedMyPage = UIImage(named: "selectedMyPage")!
+    
+    // MARK: - Icon
+    
+    public static let icnClose_ = UIImage(named: "icn_close")!
+    public static let icnExpandDown = UIImage(named: "icn_expand_down")!
+    public static let iconExpandLeft = UIImage(named: "icn_expand_left")!
+    public static let iconBell = UIImage(named: "icon_bell")!
+    public static let icnPuzzle = UIImage(named: "icon_puzzle")!
+    public static let icnWarning = UIImage(named: "icon_warning")!
+    
+    // MARK: - Rank
+    
+    public static let rank1st = UIImage(named: "rank_1st")!
+    public static let rank2nd = UIImage(named: "rank_2nd")!
+    public static let rank3rd = UIImage(named: "rank_3rd")!
 }
