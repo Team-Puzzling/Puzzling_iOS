471cbda7e030d55d3815091f32a29873db976342
Cheon, Seongwoo <cjs1399@naver.com>
2023-07-11 15:32:12 +0900

    [Featuer] #9 - 회고 작성 뷰에서 사용하는 CustomRadioButton Class 생성

---
+++ Puzzling/Puzzling/Util/CustomClass/CustomRadioButton.swift
@@ -0,0 +1,78 @@
+//
+//  UIRadioButton.swift
+//  Puzzling
+//
+//  Created by 천성우 on 2023/07/11.
+//
+
+import UIKit
+
+class CustomRadioButton: UIButton {
+    
+    private var btnSelected: Bool = false
+    
+    // MARK: - UI Components
+    
+    private let innerCircle = UIView()
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setButton()
+        setInnerCircle()
+        addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setButton()
+        setInnerCircle()
+        addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    private func setButton() {
+        self.layer.cornerRadius = 8.0
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.gray200.cgColor
+    }
+    
+    private func setInnerCircle() {
+        let circleSize = CGSize(width: 10.0, height: 10.0)
+        let circlePoint = CGPoint(
+            x: (self.bounds.width - circleSize.width) / 2,
+            y: (self.bounds.height - circleSize.height) / 2
+        )
+        let circleFrame = CGRect(origin: circlePoint, size: circleSize)
+        
+        innerCircle.frame = circleFrame
+        innerCircle.layer.cornerRadius = circleSize.width / 2
+        innerCircle.backgroundColor = UIColor.blue400
+        innerCircle.isUserInteractionEnabled = false
+        innerCircle.isHidden = true
+        
+        self.addSubview(innerCircle)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let circleSize = CGSize(width: 10.0, height: 10.0)
+        let circlePoint = CGPoint(
+            x: (self.bounds.width - circleSize.width) / 2,
+            y: (self.bounds.height - circleSize.height) / 2
+        )
+        let circleFrame = CGRect(origin: circlePoint, size: circleSize)
+        
+        innerCircle.frame = circleFrame
+        innerCircle.layer.cornerRadius = circleSize.width / 2
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc private func buttonTapped() {
+        innerCircle.isHidden = !innerCircle.isHidden
+        btnSelected = !innerCircle.isHidden
+        print(btnSelected)
+    }
+}
