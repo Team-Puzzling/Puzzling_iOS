5a0f00cfb6b5493f9b7c3fa7ac71ab424be04ba8
PecanPie <hidra0321@naver.com>
2023-07-12 04:46:16 +0900

    Added files and assets

---
+++ Puzzling/Puzzling/Presentation/Home/IndivisualDashboard/View/IndivisualCardView.swift
@@ -0,0 +1,20 @@
+//
+//  IndivisualCardView.swift
+//  Puzzling
+//
+//  Created by KYUBO A. SHIM on 2023/07/12.
+//
+
+import UIKit
+
+class IndivisualCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
