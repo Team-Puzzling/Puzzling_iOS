08f7989ebf24ca5773e9dd9921413125b274d5a0
kwonjeong <kwonjeong9470@gmail.com>
2023-07-12 03:43:40 +0900

    [Design] #11 - navigation bar 뷰로 전환

--- Puzzling/Puzzling/Presentation/CreateProject/ViewController/CreateProjectViewController.swift
+++ Puzzling/Puzzling/Presentation/CreateProject/ViewController/CreateProjectViewController.swift
@@ -15,17 +15,29 @@ final class CreateProjectViewController: UIViewController {
     
     // MARK: - UI Components
     
+    private let navigationView = UIView()
+    private let navigationTitleLabel = UILabel()
+    private let closeButton = UIButton()
+    private let nameView = InputContentView(type: .name)
+    private let descriptionView = InputContentView(type: .description)
+    private let roleView = InputContentView(type: .role)
+    private let nicknameView = InputContentView(type: .nickname)
+    
     // MARK: - Properties
     
     // MARK: - Initializer
     
     // MARK: - View Life Cycle
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
         setLayout()
-        setNavigationBar()
+//        setNavigationBar()
     }
 }
 
@@ -37,30 +49,67 @@ extension CreateProjectViewController {
         
         view.backgroundColor = .white000
         
+        navigationTitleLabel.do {
+            $0.text = "프로젝트 등록하기"
+            $0.font = .fontGuide(.heading4_kor)
+        }
+        
+        closeButton.do {
+            $0.setImage(Image.xMark, for: .normal)
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
         
+        navigationView.addSubviews(navigationTitleLabel, closeButton)
+        view.addSubviews(navigationView, nameView, descriptionView, roleView, nicknameView)
+        
+        navigationView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(48)
+        }
+        
+        navigationTitleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        closeButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(8)
+            $0.width.height.equalTo(48)
+        }
+        
+        nameView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(32)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.height.equalTo(106)
+        }
+        
+        descriptionView.snp.makeConstraints {
+            $0.top.equalTo(nameView.snp.bottom).offset(16)
+            $0.leading.trailing.equalTo(nameView)
+            $0.height.equalTo(nameView)
+        }
+        
+        roleView.snp.makeConstraints {
+            $0.top.equalTo(descriptionView.snp.bottom).offset(16)
+            $0.leading.trailing.equalTo(nameView)
+            $0.height.equalTo(nameView)
+        }
+        
+        nicknameView.snp.makeConstraints {
+            $0.top.equalTo(roleView.snp.bottom).offset(16)
+            $0.leading.trailing.equalTo(nameView)
+            $0.height.equalTo(nameView)
+        }
+        
     }
     
     // MARK: - Methods
-    
-    private func setNavigationBar() {
-        navigationController?.navigationBar.backgroundColor = .clear
-        navigationController?.navigationBar.titleTextAttributes = [
-            .foregroundColor: UIColor.black000,
-            .font: UIFont.fontGuide(.heading4_kor)
-        ]
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            image: Image.icnClose)
-//            style: .plain,
-//            target: self,
-//            action: #selector(backButtonTapped))
-        navigationItem.title = "프로젝트 등록하기"
-        navigationController?.navigationBar.tintColor = .gray500
-    }
     
     // MARK: - @objc Methods
 }
