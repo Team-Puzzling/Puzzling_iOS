0b4b7ac7ff25e963bcf2fece12c847ec3ea05cbe
PecanPie <hidra0321@naver.com>
2023-07-13 22:18:04 +0900

    [Feature] #14 - Added Card button for MainView

--- Puzzling/Puzzling/Presentation/Home/IndivisualDashboard/View/IndivisualCardButtonView.swift
+++ Puzzling/Puzzling/Presentation/Home/IndivisualDashboard/View/IndivisualCardButtonView.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class IndivisualCardButtonView: UIView {
     
     private let backgroundImageView = UIImageView()
     private let puzzleBoardLabel = UILabel()
     private let puzzleBoardCountLabel = UILabel()
     private let chevronNextImageView = UIImageView()
-    
+
     private var puzzleCount: Int = 0 {
         didSet {
             puzzleBoardCountLabel.text = "\(puzzleCount) 판"
@@ -29,7 +32,8 @@ final class IndivisualCardButtonView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.makeRounded(radius: 16)
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 16
     }
     
     required init?(coder: NSCoder) {
@@ -44,6 +48,7 @@ extension IndivisualCardButtonView {
     }
     
     private func setUI() {
+        
         backgroundImageView.do {
             $0.image = Image.indivisualCardBackground
             $0.contentMode = .scaleAspectFill
@@ -56,15 +61,16 @@ extension IndivisualCardButtonView {
         }
         
         chevronNextImageView.do {
-            $0.image = UIImage(systemName: "chevron.right")
+            let configuration = UIImage.SymbolConfiguration(pointSize: 0, weight: .semibold, scale: .medium)
+            $0.image = UIImage(systemName: "chevron.right", withConfiguration: configuration)
             $0.contentMode = .scaleAspectFit
             $0.tintColor = .gray500
         }
         
         puzzleBoardCountLabel.do {
+            $0.text = "n 판"
             $0.font = .fontGuide(.heading4_kor)
             $0.textColor = .blue400
-            $0.backgroundColor = .red
             $0.textAlignment = .right
         }
     }
@@ -73,24 +79,50 @@ extension IndivisualCardButtonView {
         self.addSubviews(backgroundImageView, puzzleBoardLabel, chevronNextImageView, puzzleBoardCountLabel)
         
         backgroundImageView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.center.equalToSuperview()
+            $0.width.height.equalToSuperview()
         }
         
-        puzzleBoardCountLabel.snp.makeConstraints {
+        puzzleBoardLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(24)
             $0.centerY.equalToSuperview()
         }
         
         chevronNextImageView.snp.makeConstraints {
-            $0.size.equalTo(12)
+            $0.size.equalTo(20)
             $0.trailing.equalToSuperview().inset(22)
             $0.centerY.equalToSuperview()
         }
         
         puzzleBoardCountLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview()
-            $0.trailing.equalTo(chevronNextImageView.snp.leading).offset(20)
+            $0.trailing.equalTo(chevronNextImageView.snp.leading).offset(-20)
             $0.width.lessThanOrEqualTo(50)
+        }
+    }
+}
+
+extension CALayer {
+    func applyCornerRadiusShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0,
+        cornerRadiusValue: CGFloat = 0)
+    {
+        cornerRadius = cornerRadiusValue
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
         }
     }
 }
