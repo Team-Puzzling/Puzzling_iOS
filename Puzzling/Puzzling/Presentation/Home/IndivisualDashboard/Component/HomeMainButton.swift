0b3fb0fafd512020f51102f15d5bc0dc22bfa05b
PecanPie <hidra0321@naver.com>
2023-07-13 22:05:24 +0900

    [Feature] #14 - Added HomeMainButton for creating retrospect

--- Puzzling/Puzzling/Presentation/Home/IndivisualDashboard/Component/HomeMainButton.swift
+++ Puzzling/Puzzling/Presentation/Home/IndivisualDashboard/Component/HomeMainButton.swift
@@ -9,12 +9,72 @@ import UIKit
 
 class HomeMainButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var isButtonEnabled: Bool = false {
+        didSet {
+            if isButtonEnabled == true {
+                self.isEnabled = true
+            } else {
+                self.isEnabled = false
+            }
+        }
     }
-    */
+    
+    enum ButtonType {
+        case notToday
+        case today
+        case done
+        
+        var buttonTitle: String {
+            switch self {
+            case .notToday:
+                return "회고 작성일이 아니에요"
+            case .today, .done:
+                return "회고 작성하기"
+            }
+        }
+        
+        var buttonColor: UIColor {
+            switch self {
+            case .notToday, .done:
+                return .gray400
+            case .today:
+                return .blue400
+            }
+        }
+    }
+    
+    private let buttonTitle: String
+    private let buttonBackgroundColor: UIColor
+    
+    init(frame: CGRect, type: ButtonType) {
+        self.buttonTitle = type.buttonTitle
+        self.buttonBackgroundColor = type.buttonColor
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension HomeMainButton {
+
+    private func setUI() {
+        self.isEnabled = false
+        self.backgroundColor = buttonBackgroundColor
+        self.layer.cornerRadius = 16
+        self.setTitle(buttonTitle, for: .normal)
+        self.titleLabel?.font = .fontGuide(.heading4_kor)
+    }
+}
+
+extension HomeMainButton {
+    func enableButton(to bool: Bool) {
+        if bool == true {
+            self.isButtonEnabled = true
+        } else {
+            self.isButtonEnabled = false
+        }
+    }
 }
