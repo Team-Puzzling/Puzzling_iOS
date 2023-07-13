70df83d755a80454b3231690c114ee9cb49333d8
PecanPie <hidra0321@naver.com>
2023-07-13 22:38:27 +0900

    Added IndivisualVC

--- Puzzling/Puzzling/Presentation/Home/IndivisualDashboard/ViewController/IndivisualDashboardViewController.swift
+++ Puzzling/Puzzling/Presentation/Home/IndivisualDashboard/ViewController/IndivisualDashboardViewController.swift
@@ -9,9 +9,60 @@ import UIKit
 
 final class IndivisualDashboardViewController: UIViewController {
 
+    private var tabBarHeight: CGFloat {
+        guard let height = self.tabBarController?.tabBar.frame.size.height else {
+            print("fffffff")
+            return 0.0
+        }
+        return height
+    }
+    
+    private let mainView: IndivisualMainBoxView = IndivisualMainBoxView()
+    private let actionPlanView = ActionPlanView()
+    private var homeMainButton = HomeMainButton(frame: .zero, type: .notToday)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDelegate()
+        setUI()
+        setLayout()
+    }
+}
+
+extension IndivisualDashboardViewController {
+    
+    private func setDelegate() {
         
-        view.backgroundColor = .red
+    }
+    
+    private func setUI() {
+        view.backgroundColor = .white000
+        
+        mainView.do {
+            $0.passPuzzleData(userName: "심규보봉", piecesCount: 5)
+        }
+    }
+    
+    private func setLayout() {
+        view.addSubviews(mainView, homeMainButton, actionPlanView)
+        
+        mainView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.height.equalToSuperview().dividedBy(2.55)
+        }
+        
+        homeMainButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(55)
+            $0.bottom.equalToSuperview().inset(tabBarHeight + 19)
+        }
+        
+        actionPlanView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalTo(mainView.snp.bottom).offset(20)
+//            $0.height.equalTo(300)
+            $0.bottom.equalTo(homeMainButton.snp.top).offset(-49)
+        }
     }
 }
