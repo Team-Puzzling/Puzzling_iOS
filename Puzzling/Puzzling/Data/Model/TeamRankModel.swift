01a4b67858e1efb92cab8431bfa937089290e36d
PecanPie <hidra0321@naver.com>
2023-07-15 23:29:45 +0900

    Added Files

---
+++ Puzzling/Puzzling/Data/Model/TeamRankModel.swift
@@ -0,0 +1,8 @@
+//
+//  TeamRankModel.swift
+//  Puzzling
+//
+//  Created by KYUBO A. SHIM on 2023/07/15.
+//
+
+import Foundation
---
+++ Puzzling/Puzzling/Presentation/Home/TeamDashboard/Component/TeamRankTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TeamRankTableViewCell.swift
+//  Puzzling
+//
+//  Created by KYUBO A. SHIM on 2023/07/15.
+//
+
+import UIKit
+
+class TeamRankTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
