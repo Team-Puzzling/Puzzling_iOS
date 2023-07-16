3d502c7ad13dfa55f9c6f7710a26bb287b977b4c
kwonjeong <kwonjeong9470@gmail.com>
2023-07-17 02:35:02 +0900

    [Add] #27 - 이미지 에셋 추가

---
+++ Puzzling/Puzzling/Presentation/InvitationCode/ViewController/InvitationCodeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InvitationCodeViewController.swift
+//  Puzzling
+//
+//  Created by KJ on 2023/07/17.
+//
+
+import UIKit
+
+class InvitationCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
