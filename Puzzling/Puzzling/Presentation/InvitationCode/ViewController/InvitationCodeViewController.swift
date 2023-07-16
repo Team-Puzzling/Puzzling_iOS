ae53194792a2bbf92897bfd791639899ae43fa68
kwonjeong <kwonjeong9470@gmail.com>
2023-07-17 03:33:31 +0900

    [Design] #27 - 초대코드 입력 뷰 UI 구현

--- Puzzling/Puzzling/Presentation/InvitationCode/ViewController/InvitationCodeViewController.swift
+++ Puzzling/Puzzling/Presentation/InvitationCode/ViewController/InvitationCodeViewController.swift
@@ -7,23 +7,112 @@
 
 import UIKit
 
-class InvitationCodeViewController: UIViewController {
+import Moya
+import SnapKit
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class InvitationCodeViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let navigationView = UIView()
+    private let navigationTitleLabel = UILabel()
+    private let closeButton = UIButton()
+    private let logoImageView = UIImageView()
+    private let invitationCodeInputView = UIView()
+    private let inputCompletionButton = CheckButton()
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
     }
-    */
+}
 
+extension InvitationCodeViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+        view.backgroundColor = .white000
+        
+        navigationView.do {
+            $0.backgroundColor = .clear
+        }
+        
+        navigationTitleLabel.do {
+            $0.text = "프로젝트 등록하기"
+            $0.font = .fontGuide(.heading4_kor)
+            $0.textColor = .black000
+        }
+        
+        closeButton.do {
+            $0.setImage(Image.xMark, for: .normal)
+        }
+        
+        logoImageView.do {
+            $0.image = Image.invitationCodeLogo
+        }
+        
+        inputCompletionButton.do {
+            $0.setTitle("입력 완료", for: .normal)
+            $0.setState(.notAllow)
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+        navigationView.addSubviews(navigationTitleLabel, closeButton)
+        view.addSubviews(navigationView, logoImageView,
+                         invitationCodeInputView, inputCompletionButton)
+        
+        navigationView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(48)
+        }
+        
+        navigationTitleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        closeButton.snp.makeConstraints {
+            $0.trailing.equalTo(view.safeAreaLayoutGuide).inset(8)
+            $0.centerY.equalToSuperview()
+        }
+        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(88)
+            $0.centerX.equalToSuperview()
+        }
+        
+        invitationCodeInputView.snp.makeConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(80)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
+        
+        inputCompletionButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(50)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.height.equalTo(55)
+        }
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
 }
