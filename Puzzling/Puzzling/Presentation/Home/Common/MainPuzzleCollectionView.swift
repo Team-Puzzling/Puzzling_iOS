c72cfab7c05bb3475ca05a86bfde1f27512a554b
PecanPie <hidra0321@naver.com>
2023-07-18 05:18:24 +0900

    [Refactor] #22 - Fixed bugs and Simplified the logic

--- Puzzling/Puzzling/Presentation/Home/Common/MainPuzzleCollectionView.swift
+++ Puzzling/Puzzling/Presentation/Home/Common/MainPuzzleCollectionView.swift
@@ -13,10 +13,9 @@ import Then
 final class MainPuzzleCollectionView: UIView {
 
     private lazy var puzzleCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.setFlowLayout())
-    private var modelType: ModelProtocol.Type!
     private var modelData: [ModelProtocol?] = []
     private var dashboardType: DashboardType
-    
+
     private let cellHeight: CGFloat = UIScreen.main.bounds.height / 9.2
     private let cellWidth: CGFloat = UIScreen.main.bounds.width / 5.86
     
@@ -27,7 +26,6 @@ final class MainPuzzleCollectionView: UIView {
         setUI()
         setLayout()
         setRegister()
-        print("opopopop")
     }
     
     required init?(coder: NSCoder) {
@@ -47,10 +45,9 @@ extension MainPuzzleCollectionView {
         puzzleCollectionView.do {
             $0.isScrollEnabled = true
             $0.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
-            $0.showsHorizontalScrollIndicator = true
+            $0.showsHorizontalScrollIndicator = false
             $0.backgroundColor = .clear
         }
-        print("6️⃣")
     }
     
     private func setLayout() {
@@ -60,16 +57,10 @@ extension MainPuzzleCollectionView {
             $0.horizontalEdges.equalToSuperview()
             $0.verticalEdges.equalToSuperview().inset(5)
         }
-        print("🔸")
     }
     
     private func setRegister() {
-        if self.modelType == IndivisualPuzzleStatusModel.self {
-            puzzleCollectionView.registerCell(MainPuzzleCollectionViewCell<IndivisualPuzzleStatusModel>.self)
-        } else if self.modelType == TeamPuzzleStatusModel.self {
-            puzzleCollectionView.registerCell(MainPuzzleCollectionViewCell<TeamPuzzleStatusModel>.self)
-        }
-        print("5️⃣")
+        puzzleCollectionView.register(MainPuzzleCollectionViewCell.self, forCellWithReuseIdentifier: dashboardType.identifier)
     }
 }
 
@@ -83,53 +74,49 @@ extension MainPuzzleCollectionView {
 }
 
 extension MainPuzzleCollectionView: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 3
-    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 15
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        print("tyutyu")
         return CGSize(width: cellWidth, height: cellHeight)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print("qqqqqq")
         switch dashboardType {
         case .indivisual:
-            let cell = collectionView.dequeueCell(type: MainPuzzleCollectionViewCell<IndivisualPuzzleStatusModel>.self, indexPath: indexPath)
-            guard let model = modelData as? [IndivisualPuzzleStatusModel] else { return UICollectionViewCell() }
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: dashboardType.identifier, for: indexPath) as? MainPuzzleCollectionViewCell else {
+                return UICollectionViewCell() }
+            guard let model = modelData as? [IndivisualPuzzleStatusModel?] else { return UICollectionViewCell() }
             cell.setDataBind(data: model[indexPath.item], index: indexPath.item)
-            print("!!!!")
             return cell
         case .team:
-            let cell = collectionView.dequeueCell(type: MainPuzzleCollectionViewCell<TeamPuzzleStatusModel>.self, indexPath: indexPath)
-            guard let model = modelData as? [TeamPuzzleStatusModel] else { return UICollectionViewCell() }
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: dashboardType.identifier, for: indexPath) as? MainPuzzleCollectionViewCell else {
+                return UICollectionViewCell() }
+            guard let model = modelData as? [TeamPuzzleStatusModel?] else { return UICollectionViewCell() }
             cell.setDataBind(data: model[indexPath.item], index: indexPath.item)
-            print("!??!")
             return cell
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? MainPuzzleCollectionViewCell else { return }        
     }
 }
 
 extension MainPuzzleCollectionView {
     func passPuzzleData(data: [ModelProtocol?]) {
+        if data.isEmpty {
+            self.modelData = Array(repeating: nil, count: 15)
+        }
+        
         let restOfDataCount = 15 - data.count
         if restOfDataCount == 0 {
             self.modelData = data
+            return
         }
         
-        if data is [IndivisualPuzzleStatusModel?] {
-            self.modelData = data + Array(repeating: nil, count: restOfDataCount)
-            self.modelType = IndivisualPuzzleStatusModel.self
-        } else if data is [TeamPuzzleStatusModel?] {
-            self.modelData = data + Array(repeating: nil, count: restOfDataCount)
-            self.modelType = TeamPuzzleStatusModel.self
-        }
-        
-        print("pppp>?!")
+        self.modelData = data + Array(repeating: nil, count: restOfDataCount)
     }
 }
