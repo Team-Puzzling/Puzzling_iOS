731b1c6e1db1578a5a2354a69cb7760819b95597
PecanPie <hidra0321@naver.com>
2023-07-17 21:37:12 +0900

    Added Model for Main Puzzle CollectiionView

--- Puzzling/Puzzling/Data/Model/TeamPuzzleStatusModel.swift
+++ Puzzling/Puzzling/Data/Model/TeamPuzzleStatusModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol ModelProtocol {}
+
+protocol TeamModelProtocol: AnyObject {
+    var status: TeamPuzzleStatusData { get set }
+    var puzzleCount: Int { get set }
+    var puzzleAssetNumber: Int { get set }
+}
+
+class TeamPuzzleStatusModel: TeamModelProtocol, ModelProtocol {
+    var status: TeamPuzzleStatusData
+    var puzzleCount: Int
+    var puzzleAssetNumber: Int
+    
+    init(status: TeamPuzzleStatusData, puzzleCount: Int, puzzleAssetNumber: Int) {
+        self.status = status
+        self.puzzleCount = puzzleCount
+        self.puzzleAssetNumber = puzzleAssetNumber
+    }
+}
+
+struct TeamPuzzleStatusData {
+    var isTodayReviewDay: Bool
+    var hasReviewed: Bool
+    var reviewedMemberPercentage: String
+    var reviewDate: String
+}
