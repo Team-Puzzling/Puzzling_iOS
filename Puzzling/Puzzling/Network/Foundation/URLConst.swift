6287d5d2af97d001f27494f46bff64b9f4b11b34
kwonjeong <kwonjeong9470@gmail.com>
2023-07-17 21:12:43 +0900

    [Fix] #35 - 서버통신 세팅 URL 수정

--- Puzzling/Puzzling/Network/Foundation/URLConst.swift
+++ Puzzling/Puzzling/Network/Foundation/URLConst.swift
@@ -15,32 +15,46 @@ public enum URLConst {
     
     //MARK: - URL Path
     
+    // Auth 관련
+    
     static let postUserURL = "/api/v1/auth"
     static let getUserTokenURL = "/api/v1/auth/token"
     
+    // 회고 작성
+    
     static let getReviewTemplateURL = "/api/v1/review/template"
-    static let postMemeberURL = "/api/v1/member"
-    static let projectURL = "/project"
-    static let postReviewTILURL = "/review/TIL"
-    static let postReivew5FURL = "/review/5F"
-    static let postReviewAAR = "/review/ARR"
-    static let getReviewPreviousTemplateURL = "/review/previous-template"
+    static let postReviewTILURL = "/api/v1/member/{memberId}/project/{projectId}/review/TIL"
+    static let postReview5FURL = "/api/v1/member/{memberId}/project/{projectId}/review/5F"
+    static let postReviewARRURL = "/api/v1/member/{memberId}/project/{projectId}/review/ARR"
+    static let getReviewPreviousTemplateURL = "/api/v1/member/{memberId}/project/{projectId}/review/previous-template"
     
-    static let postJoinURL = "/project/join"
+    // 프로젝트
+    
+    static let postProjectURL = "/api/v1/member/{memberId}/project"
+    static let postProjectJoinURL = "/api/v1/member/{merberId}/project/join"
     static let getInvitationCodeURL = "/api/v1/project/verify"
-    static let invitationCode = "invitationCode"
-    static let getCycleURL = "/cycle"
+    static let getProjectCycleURL = "/api/v1/project/{projectId}/cycle"
     
-    static let getPuzzleURL = "/puzzle"
-    static let today = "today"
-    static let getActionPlanURL = "/actionplan"
+    // 개인 회고
+    
+    static let getMemberProjectPuzzleURL = "/api/v1/member/{memberId}/project/{projectId}/puzzle"
+    static let getMemberProjectActionplanURL = "/api/v1/member/{memberId}/project/{projectId}/actionplan"
+    static let getMemberProjectAllURL = "/api/v1/member/{memberId}/project/all"
     static let getProjectAllURL = "/project/all"
     
-    static let getTeamPuzzleURL = "/team/puzzle"
-    static let getRankURL = "/rank"
+    // 팀 회고
+    
+    static let getTeamProjectPuzzleURL = "/api/v1/project/{projectId}/team/puzzle"
+    static let getProjectRankURL = "/api/v1/project/{projectId}/rank"
+    static let getProjectTeamReviewURL = "/api/v1/project/{projectId}/team/review"
+    
+    // My
+    
+    static let getMemberProjectReviewsURL = "/api/v1/member/{memberId}/project/{projectId}/reviews"
+    static let getMemberProjectReviewURL = "~/api/v1/member/{memberId}/project/{projectId}/review"
+    
     static let startDate = "startDate"
     static let endDate = "endDate"
-    
-    static let reviewsURL = "/reviews"
-    static let reviewURL = "/review"
+    static let today = "today"
+    static let invitationCode = "invitationCode"
 }
