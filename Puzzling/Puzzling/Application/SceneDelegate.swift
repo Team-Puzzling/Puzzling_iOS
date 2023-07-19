954908fe3a0a963a8c83d9373be4dd7b821834bc
frohsch <kminjoo1213@gmail.com>
2023-07-19 23:10:00 +0900

    [Chore] #41 - Chore

--- Puzzling/Puzzling/Application/SceneDelegate.swift
+++ Puzzling/Puzzling/Application/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
             
-            let vc = ReviewDetailViewController()
+            let vc = MyProjectViewController()
             let rootVC = UINavigationController(rootViewController: vc)
             window.rootViewController = rootVC
             window.makeKeyAndVisible()
