6a4718247aecc1abc0e259e6c544cd31b8f340f1
Cheon, Seongwoo <cjs1399@naver.com>
2023-07-19 19:47:32 +0900

    [Feature] #43 - Add Server Model code

---
+++ Puzzling/Puzzling/Data/Model/CreateRetrospectModel/TemplateCaseModel.swift
@@ -0,0 +1,14 @@
+//
+//  TemplateCaseModel.swift
+//  Puzzling
+//
+//  Created by 천성우 on 2023/07/18.
+//
+
+import Foundation
+
+struct TemplateCaseModel {
+    let reviewTemplateId: Int
+    let reviewTemplateName: String
+    let reviewTemplateMeaning: String
+}
