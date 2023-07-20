a2e0c65fc11622c72cd31e95d357f665ea15eebf
PecanPie <hidra0321@naver.com>
2023-07-21 01:39:20 +0900

    [Feat] #46 - Added NetworkService for getting; ProjectServiceTemporary

--- Puzzling/Puzzling/Network/APIService/ProjectServiceTemporary.swift
+++ Puzzling/Puzzling/Network/APIService/ProjectServiceTemporary.swift
@@ -7,60 +7,122 @@
 
 import UIKit
 
+import Moya
+
 protocol ProjectNetworkingProtocol: AnyObject {
     func NOTNAMEDPROPERLY()
 }
 
-final class ProjectServiceTemporary: ProjectNetworkingProtocol {
+enum ProjectServiceKBS {
+    case fetchIndivisualPuzzle(memberId: Int, projectId: Int, todayString: String)
+    case fetchActionPlans(memberId: Int, projectId: Int)
+    case fetchOngoingProjectList(memberId: Int)
+    case fetchTeamPuzzle(projectId: Int, todayString: String)
+    case fetchTeamRankTable(projectId: Int)
+    case fetchRetrospectCycle(projectId: Int)
+}
+
+extension ProjectServiceKBS: TargetType {
+    var baseURL: URL {
+        guard let url = URL(string: "http://3.39.174.107") else {
+            print("Failed converting URL")
+            return URL(string: "")! }
+        return url
+    }
     
-    private var dummyData: [TeamRankModel] = [TeamRankModel(userName: "aa", role: "iOS Dev", pieces: 17), TeamRankModel(userName: "bb", role: "PM", pieces: 12), TeamRankModel(userName: "cc", role: "AOS Dev", pieces: 14), TeamRankModel(userName: "dd", role: "Server-T", pieces: 9), TeamRankModel(userName: "ee", role: "AOS Dev", pieces: 40), TeamRankModel(userName: "ff", role: "iOS Dev", pieces: 22), TeamRankModel(userName: "gg", role: "UXUI Designer", pieces: 14)]
+    var path: String {
+        switch self {
+        case .fetchIndivisualPuzzle(let memberId, let projectId, let todayString):
+            return URLConst.getMemberProjectPuzzleURL.replacingOccurrences(of: "{memberId}", with: "\(memberId)").replacingOccurrences(of: "{projectId}", with: "\(projectId)")
+        case .fetchActionPlans(let memberId, let projectId):
+            return URLConst.getMemberProjectActionplanURL.replacingOccurrences(of: "{memberId}", with: "\(memberId)").replacingOccurrences(of: "{projectId}", with: "\(projectId)")
+        case .fetchOngoingProjectList(let memberId):
+            return URLConst.getMemberProjectAllURL.replacingOccurrences(of: "{memberId}", with: "\(memberId)")
+        case .fetchTeamPuzzle(let projectId, let todayString):
+            return URLConst.getTeamProjectPuzzleURL.replacingOccurrences(of: "{projectId}", with: "\(projectId)")
+        case .fetchTeamRankTable(let projectId):
+            return URLConst.getProjectRankURL.replacingOccurrences(of: "{projectId}", with: "\(projectId)")
+        case .fetchRetrospectCycle(projectId: let projectId):
+            return URLConst.getProjectCycleURL.replacingOccurrences(of: "{projectId}", with: "\(projectId)")
+        }
+    }
     
-    private var dummyIndivisualPuzzleData: [IndivisualPuzzleStatusModel] = [IndivisualPuzzleStatusModel(reviewId: 1, status: .init(isTodayReviewDay: false, hasReviewed: true, reviewDate: "2023-07-01"), puzzleAssetNumber: 2), IndivisualPuzzleStatusModel(reviewId: 2, status: .init(isTodayReviewDay: false, hasReviewed: true, reviewDate: "2023-07-07"), puzzleAssetNumber: 4), IndivisualPuzzleStatusModel(reviewId: 3, status: .init(isTodayReviewDay: false, hasReviewed: true, reviewDate: "2023-07-11"), puzzleAssetNumber: 7), IndivisualPuzzleStatusModel(reviewId: 4, status: .init(isTodayReviewDay: true, hasReviewed: false, reviewDate: "2023-07-19"), puzzleAssetNumber: 3)]
+    var method: Moya.Method {
+        switch self {
+        case .fetchIndivisualPuzzle:
+            return .get
+        case .fetchActionPlans:
+            return .get
+        case .fetchOngoingProjectList:
+            return .get
+        case .fetchTeamPuzzle:
+            return .get
+        case .fetchTeamRankTable:
+            return .get
+        case .fetchRetrospectCycle:
+            return .get
+        }
+    }
     
-    private var dummyTeamPuzzleData: [TeamPuzzleStatusModel] = [TeamPuzzleStatusModel(status: .init(isTodayReviewDay: false, hasReviewed: true, reviewedMemberPercentage: "A", reviewDate: "2023-07-01"), puzzleCount: 5, puzzleAssetNumber: 4), TeamPuzzleStatusModel(status: .init(isTodayReviewDay: false, hasReviewed: true, reviewedMemberPercentage: "B", reviewDate: "2023-07-07"), puzzleCount: 8, puzzleAssetNumber: 4), TeamPuzzleStatusModel(status: .init(isTodayReviewDay: false, hasReviewed: true, reviewedMemberPercentage: "C", reviewDate: "2023-07-11"), puzzleCount: 4, puzzleAssetNumber: 4), TeamPuzzleStatusModel(status: .init(isTodayReviewDay: true, hasReviewed: true, reviewedMemberPercentage: "D", reviewDate: "2023-07-19"), puzzleCount: 2, puzzleAssetNumber: 4)]
+    var task: Moya.Task {
+        switch self {
+        case .fetchIndivisualPuzzle(_, _, let todayString):
+            let parameter: [String: Any] = [URLConst.today: todayString]
+            return .requestParameters(parameters: parameter, encoding: URLEncoding.default)
+        case .fetchActionPlans:
+            return .requestPlain
+        case .fetchOngoingProjectList:
+            return .requestPlain
+        case .fetchTeamPuzzle(_, let todayString):
+            let parameter: [String: Any] = [URLConst.today: todayString]
+            return .requestParameters(parameters: parameter, encoding: URLEncoding.default)
+        case .fetchTeamRankTable:
+            return .requestPlain
+        case .fetchRetrospectCycle:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return APIConstants.headerWithAuthorization
+    }
+    
+//    var authorizationType: AuthorizationType? {
+//        return .bearer
+//    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     func NOTNAMEDPROPERLY() {
         
-    }
-}
-
-extension ProjectServiceTemporary {
-    func getSortedData() -> [TeamRankModel] {
-        let sortedArray = self.dummyData.sorted {
-            ($1.pieces, $0.userName) < ($0.pieces, $1.userName)
-        }
-        return sortedArray
-    }
-    
-    func getTopThreeData() -> [TeamRankTopThreeModel] {
-        let sortedData = self.getSortedData()
-        var topThreeData: [TeamRankTopThreeModel] = []
-        if sortedData.isEmpty {
-            // 비어있을 때의 화면 UI 를 받으면 처리하기
-            return []
-        }
-        
-        if sortedData.count > 2 {
-            for rank in 0...2 {
-                topThreeData.append(.init(rankNumber: rank + 1, userInformation: sortedData[rank]))
-            }
-            print()
-        } else {
-            for rank in 0...sortedData.count {
-                topThreeData.append(.init(rankNumber: rank + 1, userInformation: sortedData[rank]))
-            }
-        }
-        return topThreeData
-    }
-}
-
-extension ProjectServiceTemporary {
-    func getPuzzleData(type: DashboardType) -> [ModelProtocol] {
-        switch type {
-        case .indivisual:
-            return dummyIndivisualPuzzleData
-        case .team:
-            return dummyTeamPuzzleData
-        }
     }
 }
