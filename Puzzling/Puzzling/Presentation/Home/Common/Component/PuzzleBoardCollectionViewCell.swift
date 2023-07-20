71f86660d14ed01c8f4d489abf927083e28140c9
PecanPie <hidra0321@naver.com>
2023-07-21 01:43:02 +0900

    [Feat] #46 - Added PuzzleBoardView for Each Dashboard

--- Puzzling/Puzzling/Presentation/Home/Common/Component/PuzzleBoardCollectionViewCell.swift
+++ Puzzling/Puzzling/Presentation/Home/Common/Component/PuzzleBoardCollectionViewCell.swift
@@ -7,6 +7,62 @@
 
 import UIKit
 
-class PuzzleBoardCollectionViewCell: UICollectionViewCell {
+import SnapKit
+import Then
+
+struct PuzzleBoardData {
+    var date: String
+    var imageName: String
+}
+
+final class PuzzleBoardCollectionViewCell: UICollectionViewCell {
     
+    private let dateLabel = UILabel()
+    private let puzzleImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PuzzleBoardCollectionViewCell {
+    
+    private func setUI() {
+        dateLabel.do {
+            $0.font = .fontGuide(.body2_bold_kor)
+            $0.textColor = .gray700
+        }
+        
+        puzzleImageView.do {
+            $0.contentMode = .scaleAspectFit
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubviews(dateLabel, puzzleImageView)
+        
+        dateLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(28)
+            $0.top.equalToSuperview()
+        }
+        
+        puzzleImageView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.top.equalTo(dateLabel.snp.bottom).offset(8)
+            $0.height.equalTo(puzzleImageView.snp.width).dividedBy(1.55)
+        }
+    }
+}
+
+extension PuzzleBoardCollectionViewCell {
+    func passPuzzleInformation(dateString: String, puzzleImageName: String) {
+        self.dateLabel.text = dateString
+        self.puzzleImageView.image = UIImage(named: puzzleImageName)
+    }
 }
--- Puzzling/Puzzling/Presentation/Home/Common/ViewController/PuzzleBoardViewController.swift
+++ Puzzling/Puzzling/Presentation/Home/Common/ViewController/PuzzleBoardViewController.swift
@@ -7,23 +7,143 @@
 
 import UIKit
 
-class PuzzleBoardViewController: UIViewController {
+import SnapKit
+import Then
 
+final class PuzzleBoardViewController: UIViewController, UIGestureRecognizerDelegate {
+
+    private var puzzleBoardCount: Int = 0
+    
+    private let emptyLabel = UILabel()
+    private var puzzleBoardArray: [PuzzleBoardData] = []
+    private var navigationTitle: String = ""
+    private lazy var puzzleCollectionView = UICollectionView(frame: .zero, collectionViewLayout: setFlowLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setDelegate()
+        setUI()
+        setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+        navigationController?.isNavigationBarHidden = false
+        navigationItem.titleView = self.attributeTitleView()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: Image.chevronBackIcon,
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        
+        navigationItem.leftBarButtonItem?.tintColor = .gray500
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(false)
+        tabBarController?.tabBar.isHidden = false
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    deinit {
+        print(className)
+    }
+}
 
+extension PuzzleBoardViewController {
+    
+    private func setDelegate() {
+        puzzleCollectionView.delegate = self
+        puzzleCollectionView.dataSource = self
+    }
+    
+    private func setUI() {
+        view.backgroundColor = .white000
+        
+        puzzleCollectionView.do {
+            $0.registerCell(PuzzleBoardCollectionViewCell.self)
+            $0.backgroundColor = .clear
+        }
+        
+        emptyLabel.do {
+            $0.text = "완성한 퍼즐판이 없어요!"
+            $0.font = .fontGuide(.body1_bold_kor)
+            $0.textColor = .gray400
+        }
+    }
+    
+    private func setLayout() {
+        view.addSubviews(puzzleCollectionView, emptyLabel)
+        
+        puzzleCollectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(24)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+        
+        emptyLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
+
+extension PuzzleBoardViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return puzzleBoardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueCell(type: PuzzleBoardCollectionViewCell.self, indexPath: indexPath)
+        cell.passPuzzleInformation(dateString: puzzleBoardArray[indexPath.item].date, puzzleImageName: puzzleBoardArray[indexPath.item].imageName)
+        return cell
+    }
+}
+
+extension PuzzleBoardViewController {
+    private func setFlowLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 30
+        layout.itemSize = CGSize(width: view.frame.width, height: view.frame.height / 3.6)
+        layout.scrollDirection = .vertical
+        return layout
+    }
+    
+    private func attributeTitleView() -> UIView {
+        let label: UILabel = UILabel()
+        let title: NSMutableAttributedString =
+        NSMutableAttributedString(string: self.navigationTitle, attributes: [
+            .foregroundColor: UIColor.black000,
+            .font: UIFont.fontGuide(.heading4_kor)
+        ])
+        
+        let naviTitle: NSMutableAttributedString
+        = title
+        label.attributedText = naviTitle
+        
+        return label
+    }
+}
+
+extension PuzzleBoardViewController {
+    func passNavigationTitle(boardType: DashboardType) {
+        self.navigationTitle = boardType.navigationTitle
+    }
+    
+    func passPuzzleBoardCount(count: Int) {
+        let defaultArray: [PuzzleBoardData] = [PuzzleBoardData(date: "2023.06.18. ~ 2023.07.02.", imageName: "puzzleBoard1"), PuzzleBoardData(date: "2023.07.03. ~ 2023.07.18.", imageName: "puzzleBoard2"), PuzzleBoardData(date: "2023.07.19. ~ 2023.08.02.", imageName: "puzzleBoard3")]
+        var array: [PuzzleBoardData] = []
+        for item in 0..<count {
+            array.append(defaultArray[item])
+        }
+        self.puzzleBoardArray = array
+        self.puzzleBoardCount = array.count
+        
+        if array.count > 0 {
+            self.emptyLabel.alpha = 0
+            self.emptyLabel.removeFromSuperview()
+        }
+    }
 }
