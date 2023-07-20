f9d4db4c06160a70477cb92e7ece9f002a0adc63
kwonjeong <kwonjeong9470@gmail.com>
2023-07-20 21:22:59 +0900

    [Chore] #33 - sceneDelegate 변경

--- Puzzling/Puzzling/Application/SceneDelegate.swift
+++ Puzzling/Puzzling/Application/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
 
-            let vc = CreateProjectViewController() // ReviewDetailViewController()
+            let vc = testVir // CreateProjectViewController() // ReviewDetailViewController()
             let rootVC = UINavigationController(rootViewController: vc)
             window.rootViewController = rootVC
             window.makeKeyAndVisible()
